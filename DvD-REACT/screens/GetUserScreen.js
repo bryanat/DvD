aca665a7ce97178e3c7cd1ae93ae72955ff08e70
bryanat <boyettbryan@gmail.com>
2022-09-22 07:02:01 -0400

    Refactor to remove AxiosButtonGet

--- DvD-REACT/screens/GetUserScreen.js
+++ DvD-REACT/screens/GetUserScreen.js
@@ -1,25 +1,45 @@
-// Get User History ~ similar to Personal Challenge screen
-// may remove this screen as its basically just a long term view of Personal Challenge screen
-// can put the long term view in Personal Challenge which also lets you see past challenge snapshots (final summaries)
-
-
-//// move AxiosButtonGet into here and delete AxiosButtonGet component
-
-
-import { StyleSheet } from 'react-native';
+import * as React from 'react'
+import { StyleSheet, Pressable } from 'react-native'
 import { Text, View } from '../components/Themed';
-import AxiosButtonGet from '../components/AxiosButtonGet';
-import AxiosButtonPut from '../components/AxiosButtonPut';
+import axios from 'axios'
+
 
 export default function TabOneScreen() {
+  const [axiosState, setAxiosState] = React.useState("Axios get with email data")
+
+  // express api endpoint that axios is hitting
+  const uri = "http://192.168.1.214:8088/logins/getLogin"
+  const title = "/logins/getLogin"
+
+  function axiosPressFunction() {
+    //axios.get(uri, {
+    axios({
+      method: 'put',
+      url: uri,
+      data: {
+        // <TextInput> to [emailState] to axios.data.email
+        email: 'user1111@gmail.com',
+        // <TextInput> to [passwordState] to axios.data.password
+        password: 'cutieY',
+      },
+    })
+      .then( function (response) {
+        console.log(response.data)
+        setAxiosState(`Route ${uri} returned: ${response.data.email}`)
+      })
+      .catch( function (error) {
+        console.log(error)
+      })
+  }
+
   return (
     <View style={styles.topView}>
       <Text style={styles.topText}>Users Test Routes</Text>
-      <AxiosButtonGet title="/users/getUser" uri="http://192.168.1.214:8088/users/getUser" />
-      <AxiosButtonGet title="/logins/getLogin" uri="http://192.168.1.214:8088/logins/getLogin" />
-      <AxiosButtonGet title="/logins/putLoginTest" uri="http://192.168.1.214:8088/logins/putLoginTest" />
+
+      <Pressable onPress={axiosPressFunction}><Text>{title}</Text></Pressable>
+      <Text>{axiosState}</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
