9a9d34cfabc7fb6357f14e621a617a41e1e26412
bryanat <boyettbryan@gmail.com>
2022-11-20 21:53:59 -0500

    Add layout and styling to user profile

--- DvD-REACT/screens/personalchallenge/PersonalProfile.js
+++ DvD-REACT/screens/personalchallenge/PersonalProfile.js
@@ -1,11 +1,12 @@
 // Personal Challenge
 
 import * as React from 'react'
-import { StyleSheet, Pressable } from 'react-native';
+import { StyleSheet, Pressable, Image } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import { AuthContext } from '../../hooks/AuthProvider';
 import axios from 'axios'
 import { localIpAddress } from '../../constants/Network';
+import { MaterialCommunityIcons, Foundation } from '@expo/vector-icons';
 
 // user health stats (diet, charts, bmi, calorie history)
 export default function PersonalProfile({ navigation }) {
@@ -43,16 +44,33 @@ export default function PersonalProfile({ navigation }) {
 
   return (
     <View style={styles.topView}>
-      <Text style={styles.topText}>Personal Profile</Text>
+      <View style={{alignItems: 'center'}}>
+        <Image style={styles.avatarImage} source={require('../../assets/images/a87sxszn.jpg')}/>
+        <Text style={styles.nameText}>{userdataState.name}</Text>
+      </View>
+      <View style={styles.heightweightView}>
+        <View style={styles.heightweightSubview}>
+          <MaterialCommunityIcons name="scale-bathroom" size={32} color="white" />
+          <Text style={styles.heightweightLabelText}>Weight</Text>
+          <Text style={styles.heightweightAmountText}>{userdataState.weight} lbs</Text>
+        </View>
+        <View style={styles.heightweightSubview}>
+          <MaterialCommunityIcons name="human-male-height-variant" size={32} color="white" />
+          <Text style={styles.heightweightLabelText}>Height</Text>
+          <Text style={styles.heightweightAmountText}>{userdataState.height} in</Text>
+        </View>
+        <View style={styles.heightweightSubview}>
+          {userdataState.gender == 'male' ? <Foundation name="male" size={32} color="white" /> : <Foundation name="female" size={32} color="white" /> }
+          <Text style={styles.heightweightLabelText}>Gender</Text>
+          <Text style={styles.heightweightAmountText}>{userdataState.gender}</Text>
+        </View>
+      </View>
       <Pressable onPress={pressUpdateUserdata} style={styles.pressableStyle}>
         <Text>Update User Data (dev test button)</Text>
       </Pressable>
       <Text style={styles.topText}>id: {userdataState._id}</Text>
-      <Text style={styles.topText}>Name: {userdataState.name}</Text>
-      <Text style={styles.topText}>Height: {userdataState.height}</Text>
-      <Text style={styles.topText}>Weight: {userdataState.weight}</Text>
-      <Text style={styles.topText}>Goal Weight: {userdataState.goalweight}</Text>
       <Text style={styles.topText}>Goal: {userdataState.goal}</Text>
+      <Text style={styles.topText}>Goal Weight: {userdataState.goalweight}</Text>
     </View>
   );
 }
@@ -60,6 +78,31 @@ export default function PersonalProfile({ navigation }) {
 const styles = StyleSheet.create({
   topView: {
     flex: 1,
+  },
+  avatarImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50/2,
+  },
+  nameText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  heightweightView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  heightweightLabelText: {
+    fontSize: 16,
+  },
+  heightweightAmountText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  heightweightSubview: {
+    flex: 1,
+    alignItems: 'center',
   },
   topText: {
     fontSize: 20,
@@ -76,3 +119,4 @@ const styles = StyleSheet.create({
     padding:10 
   },
 });
+
